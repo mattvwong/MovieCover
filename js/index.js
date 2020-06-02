8477510a0004bd29281e61ecf9567e54f307dac4
Mathew Wong <61606615+mattvwong@users.noreply.github.com>
2020-06-02 15:19:02 +1000

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+  //enter a movie title
+  //click search
+  //get the value of the user's search
+  //pass to our api
+  //clear search
+
+  $("form").submit(function () {
+    event.preventDefault();
+
+    var usersSearch = $("#search").val();
+    var options = {
+      type: "GET",
+      url: "http://www.omdbapi.com",
+      data: {
+        apikey: "54f3ec2a",
+        s: usersSearch,
+      },
+    };
+
+    $.ajax(options).done(displaySearches);
+  });
+
+  var displaySearches = function (data) {
+    $("ul").empty();
+    $("#search").val("");
+
+    data.Search.forEach(function (movie) {
+      var movieImage = $("<img>").attr("src", movie.Poster);
+
+      var listItem = $("<li>").append(movieImage);
+
+      $("ul").append(listItem);
+    });
+  };
+});
